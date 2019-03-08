f0ddc8cb1d5f033f7cced785ff3002147239d156
Aaron Ault <ault@purdue.edu>
2019-03-07 20:35:06 -0500

    Updates

---
+++ precache-manifest.9b59ff5998ca58bc7144ea96dc0d3689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bfb87787e62227f936d",
+    "url": "/af-monorepo/static/css/main.c9ba29fc.chunk.css"
+  },
+  {
+    "revision": "1bfb87787e62227f936d",
+    "url": "/af-monorepo/static/js/main.1bfb8778.chunk.js"
+  },
+  {
+    "revision": "8bcdc75d4f0e42a6ab6a",
+    "url": "/af-monorepo/static/css/1.cc2981c8.chunk.css"
+  },
+  {
+    "revision": "8bcdc75d4f0e42a6ab6a",
+    "url": "/af-monorepo/static/js/1.8bcdc75d.chunk.js"
+  },
+  {
+    "revision": "8928e9c443cf37876068",
+    "url": "/af-monorepo/static/js/runtime~main.8928e9c4.js"
+  },
+  {
+    "revision": "98c93ee7104ffbbeeb56bc8ad6b9bdf3",
+    "url": "/af-monorepo/index.html"
+  }
+];
